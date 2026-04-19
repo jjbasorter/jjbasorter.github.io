75a85d688a71b87efb44bb66492cb416cd4f0d58
Cori Zeps <lesisenn66@gmail.com>
2026-04-19 12:36:28 +0530

    fix

--- src/js/data/u.js
+++ src/js/data/u.js
@@ -24,63 +24,63 @@ dataSet[dataSetVersion].options = [
 dataSet[dataSetVersion].characterData = [
   {
     name: "Jonathan Joestar",
-    img: "https://static.jojowiki.com/images/b/bd/latest/20221006234855/Jonathan_Infobox_Manga.png",
+    imgUrls: ["https://static.jojowiki.com/images/b/bd/latest/20221006234855/Jonathan_Infobox_Manga.png"],
     opts: {
       series: ["PB"]
     }
   },
   {
     name: "Joseph Joestar",
-    img: "https://static.jojowiki.com/images/thumb/e/e2/latest/20221006235618/Joseph_Joestar_Infobox_Manga.png/600px-Joseph_Joestar_Infobox_Manga.png",
+    imgUrls: ["https://static.jojowiki.com/images/e/e2/latest/20221006235618/Joseph_Joestar_Infobox_Manga.png"],
     opts:{
       series: ["BT"]
     }
   },
   {
     name: "Jotaro Kujo",
-    img: "https://static.jojowiki.com/images/6/69/latest/20201130220440/Jotaro_SC_Infobox_Manga.png",
+    imgUrls: ["https://static.jojowiki.com/images/6/69/latest/20201130220440/Jotaro_SC_Infobox_Manga.png"],
     opts:{
       series: ["SDC"]
     }
   },
   {
     name: "Josuke Higashikata",
-    img: "https://static.jojowiki.com/images/a/a1/latest/20221007024100/Josuke_DU_Infobox_Manga.png",
+    imgUrls: ["https://static.jojowiki.com/images/a/a1/latest/20221007024100/Josuke_DU_Infobox_Manga.png"],
     opts:{
       series: ["DIU"]
     }
   },
   {
     name: "Giorno Giovanna",
-    img: "https://static.jojowiki.com/images/2/21/latest/20210313222135/Giorno_Giovanna_Infobox_Manga.png",
+    imgUrls: ["https://static.jojowiki.com/images/2/21/latest/20210313222135/Giorno_Giovanna_Infobox_Manga.png"],
     opts:{
       series: ["VA"]
     }
   },
   {
     name: "Jolyne Cujoh",
-    img: "https://static.jojowiki.com/images/2/20/latest/20200923041552/Jolyne_Infobox_Manga.png",
+    imgUrls: ["https://static.jojowiki.com/images/2/20/latest/20200923041552/Jolyne_Infobox_Manga.png"],
     opts:{
       series: ["SO"]
     }
   },
   {
     name: "Johnny Joestar",
-    img: "https://static.jojowiki.com/images/b/b7/latest/20230129074253/Johnny_Joestar_Infobox_Manga.png",
+    imgUrls: ["https://static.jojowiki.com/images/b/b7/latest/20230129074253/Johnny_Joestar_Infobox_Manga.png"],
     opts:{
       series: ["SBR"]
     }
   },
   {
     name: "Josuke Higashikata (JJL)",
-    img: "https://static.jojowiki.com/images/3/3a/latest/20210617175837/Josuke_JJL_Infobox_Manga.png",
+    imgUrls: ["https://static.jojowiki.com/images/3/3a/latest/20210617175837/Josuke_JJL_Infobox_Manga.png"],
     opts:{
       series: ["JJL"]
     }
   },
   {
     name: "Jodio Joestar",
-    img: "https://static.jojowiki.com/images/8/86/latest/20240714175735/Jodio_Joestar_Infobox_Manga.png",
+    imgUrls: ["https://static.jojowiki.com/images/8/86/latest/20240714175735/Jodio_Joestar_Infobox_Manga.png"],
     opts:{
       series: ["TJL"]
     }
